6c1bca8870d0110fd2eb15acfa999da1a634b304
sehyoun <lyutvs@gmail.com>
2021-09-03 23:14:03 +0900

    ⚡️ :: TestRequest

--- service-two/src/main/java/com/example/servicetwo/test/TestRequest.java
+++ service-two/src/main/java/com/example/servicetwo/test/TestRequest.java
@@ -1,2 +1,16 @@
-package com.example.servicetwo.test;public class TestRequest {
+package com.example.servicetwo.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TestRequest {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int grade;
+    private int classroom;
+    private int number;
+
 }
